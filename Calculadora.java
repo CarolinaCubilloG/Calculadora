5b8d48c8af8683013b80486dc91d6635e219b4f3
CarolinaCubilloG <carolina.cubillo@murciaeduca.es>
2024-12-12 06:55:14 -0500

    Creado main y operadores

---
+++ Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public static void main(String[] args) {
+                // Valores predefinidos (puedes cambiarlos aquí)
+                double num1 = 10.5;
+                char operador = '+';
+                double num2 = 3.2;
+        
+    }
+}
